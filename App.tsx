c7b57f5e151d1ec4335f6c92b3b9bd64d16c8661
miball0202 <aisacart@gmail.com>
2022-05-08 21:25:39 +0900

    Define outline

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Center, NativeBaseProvider } from 'native-base';
+import {
+  VStack,
+  NativeBaseProvider,
+  Box,
+} from 'native-base';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Center />
+      <Box safeAreaTop />
+      <Box flex={1} bg="coolGray.600" />
+      <Box flex={2} bg="coolGray.800">
+        <VStack bg="indigo.500" flex={1} mx={3} mt={3} borderTopRadius={25} />
+      </Box>
     </NativeBaseProvider>
   );
 }
